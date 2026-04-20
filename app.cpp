379feb4342d55716599d73718b92865b9b1510f6
kaiser-114 <areezilla@gmail.com>
2026-04-20 10:51:59 +0700

    menambahkan library

--- app.cpp
+++ app.cpp
@@ -1,27 +1,3 @@
 #include <iostream>
 using  namespace std;
 
-class Mahasiswa{
-    
-    public :
-        int nim;
-        string nama;
-        float nilai;
-
-        void printData(){
-            cout << "NIM : " << nim << endl;
-            cout << "Nama : " << nama << endl;
-            cout << "Nilai : " << nilai << endl;
-        }
-}; //batas class
-
-int main(){
-    Mahasiswa mhs;
-    mhs.nim = 2023;
-    mhs.nama = "Abra";
-    mhs.nilai = 85.5;
-
-    mhs.printData();
-
-}
-
